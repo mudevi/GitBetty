a052f349c3f1573695abeeff28c9438737101f2c
mudevi <mudevi@gmail.com>
2015-08-07 21:14:07 -0400

    first commit

---
+++ wp-content/themes/betty-cast/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+				 <p>Copyright &copy; <?php echo date('Y'); ?> <span class="middot">&middot; </span><span class="creds-break"><br /></span><a href="http://searchingforbetty.com/" >Studio 77 Productions Ltd.</a></p>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
